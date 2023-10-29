96e5042dbf91f355d08123ea4866b2d37ee02e6e
Amaya <98649425+AkameTheCoder@users.noreply.github.com>
2023-10-29 12:11:06 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "compiling, please be patient."
-
 # Compile Lua source files and create object files
 for file in lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c loadlib.c linit.c; do
-    echo "$file creating object files..."
+    echo "$file creating objects.."
+    echo "created ${file%.c}.o" 
     gcc -c "$file" -o "${file%.c}.o" -fPIC
 done
 
@@ -12,9 +11,9 @@ done
 ar r liblua.a *.o
 
 # Create a shared library liblua.so from the object files
-ld *.o -o liblua.so -shared
+gcc -shared -o liblua.so *.o
 
-# Compile lua.c and link it with liblua.a to create the Lua interpreter
-gcc lua.c -o lua -lm -L. -llua
-
-echo "Done."
+# Copy the Lua executable to /usr/local/bin
+echo "if prompted, enter password to copy to /usr/local/bin"
+sudo cp lua /usr/local/bin/
+echo "done!"
